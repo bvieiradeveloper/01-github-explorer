8c63045c54dabf0ca7a1661249819e47b4b85c71
Bruno Vieira <b.vieira@reply.com>
2022-04-09 14:46:09 -0300

    Config Babel

---
+++ dist/bundle.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _user$name;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+}
+
+var user = {
+  name: 'UserName'
+};
+console.log((_user$name = user.name) === null || _user$name === void 0 ? void 0 : _user$name.street);
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+  return <h1>Hello World</h1>
+}
+const user = {
+  name: 'UserName'
+}
+
+console.log(user.name?.street)
