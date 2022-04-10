4ee0d253aef1149732070f786adbc38e1222a132
Bruno Vieira <b.vieira@reply.com>
2022-04-10 17:41:32 -0300

    SCSS Repository-List Style

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,8 @@
 
-import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositortList'
 import  './styles/global.scss'
 
 export function App(){
   return (
-  <>
     <RepositoryList/>
-    <Counter/>
-  </>
 )}
