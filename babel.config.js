d6fac0dbc6b417b4429b2d98e1054b1d93755f00
Bruno Vieira <b.vieira@reply.com>
2022-04-12 20:14:53 -0300

    TypeScript Configuration Corrections

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ const { runtime } = require('webpack')
 module.exports = {
   presets: [
     '@babel/preset-env',
+    '@babel/preset-typescript',
     //Automatic insert report react
     ['@babel/preset-react', { runtime: 'automatic' }]
   ]
--- src/index.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { App } from './app'
-import { render } from 'react-dom'
-render(<App/>,document.getElementById('root'))
